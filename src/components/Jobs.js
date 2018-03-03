41e8ea650d74003f37e8b95fc6925541c628f959
Susana Han <susanahan@ac.c4q.nyc>
2018-03-03 17:53:29 -0500

    removing/combing code for jobs webpage

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Jobs = ({bronx, ny, brooklyn, queens, manhattan}) =>{
+// const Jobs = ({bronx, ny, brooklyn, queens, manhattan}) =>{
+class Jobs extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          Bronx:[],
+          Brooklyn: [],
+          'New York': [],
+          Manhattan: [],
+          Queens: [],
+          'Staten Island': [],
+          'Long Island  City': []
+        }
+      }
 
+      dataJobs=()=> {
+        fetch(`https://data.cityofnewyork.us/resource/6fic-ympf.json?$limit=19`)
+                .then(response=>{
+            return response.json()
+        })
+        .then(data => {
+            console.log('JOHN FETCH Jobs: ', data)
+            this.setState({
+            dataJob: data
+            }) 
+        this.boroughComunity();
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+      
+      boroughComunity = () =>{
+        let boroughs = [];
+        if(this.state.dataJob.length > 0){
+          this.state.dataJob.forEach(job=>{
+            if(!boroughs.includes(job.borough_community)){
+              boroughs.push(job.borough_community);
+            };
+          });
+          this.setState({boroughs: boroughs});
+          this.state.dataJob.map(job=>{
+            boroughs.map(b=>{
+              if(job.borough_community === b){
+                this.setState({
+                  b: this.state[b].push(job)
+                });
+              }
+            })
+          })
+        };
+    
+      };
+
+    componentDidMount=() => {
+        this.dataJobs()
+    }
+
+render() {  
+    const NY = this.state['New York']
+    const SI = this.state['Staten Island']
+    const LIC = this.state['Long Island City']
+    const {Bronx, Brooklyn, Manhattan, Queens, } =this.state
     return(
         <div id="container-jobs">
             <h1>Bronx Jobs and Internships</h1>
             <div className="bronx">
-                {bronx.map(job=>(
+                {Bronx.map(job=>(
                     <div className='jobPost'>
                     <p>
                         {job.agency}
@@ -22,7 +83,7 @@ const Jobs = ({bronx, ny, brooklyn, queens, manhattan}) =>{
             </div>
             <h1>New York Jobs and Internships</h1>
             <div className="NY">
-                {ny.map(job=>(
+                {NY.map(job=>(
                     <div className='jobPost'>
                     <p>
                         {job.agency}
@@ -38,7 +99,7 @@ const Jobs = ({bronx, ny, brooklyn, queens, manhattan}) =>{
             </div>
             <h1>Manhattan Jobs and Internships</h1>
             <div className="manhattan">
-                {manhattan.map(job=>(
+                {Manhattan.map(job=>(
                     <div className='jobPost'>
                     <p>
                         {job.agency}
@@ -54,7 +115,7 @@ const Jobs = ({bronx, ny, brooklyn, queens, manhattan}) =>{
             </div>
             <h1>Queens Jobs and Internships</h1>
             <div className="queens">
-                {queens.map(job=>(
+                {Queens.map(job=>(
                     <div className='jobPost'>
                     <p>
                         {job.agency}
@@ -70,7 +131,7 @@ const Jobs = ({bronx, ny, brooklyn, queens, manhattan}) =>{
             </div>
             <h1>Brooklyn Jobs and Internships</h1>
             <div className="brooklyn">
-                {brooklyn.map(job=>(
+                {Brooklyn.map(job=>(
                     <div className='jobPost'>
                     <p>
                         {job.agency}
@@ -87,6 +148,7 @@ const Jobs = ({bronx, ny, brooklyn, queens, manhattan}) =>{
                         
         </div>
     )
-}
+    }
 
+}
 export default Jobs;

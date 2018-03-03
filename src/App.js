bf9af451f0c49c8cc621cc68ca0fd0e854c0cf90
Susana Han <susanahan@ac.c4q.nyc>
2018-03-03 11:49:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Welcome to Our Page</h1>
+        <p> type stuff        </p>
+      </div>
+    );
+  }
+}
+
+export default App;

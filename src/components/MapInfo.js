5665f25a25299b12ea731596025312cef8008f63
Susana Han <susanahan@ac.c4q.nyc>
2018-03-05 15:13:02 -0500

    changing style css

--- src/components/MapInfo.js
+++ src/components/MapInfo.js
@@ -3,6 +3,7 @@ import React from "react";
 const MapInfo = spot => {
   return (
     <div>
+      <p> Jobs & Internships: </p> 
       Agency: {spot.agency}<br />
       Address: {spot.address} <br />
       Zip: {spot.location_1_zip}
